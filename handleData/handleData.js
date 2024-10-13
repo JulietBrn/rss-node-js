3da5c1ea28c14f20210e368b30ad5a226c6e8355
JulietBrn <shishkina.ja@gmail.com>
2024-10-13 15:02:36 +0200

    feat: implement printing list of files

--- handleData/handleData.js
+++ handleData/handleData.js
@@ -1,29 +1,36 @@
 import { calculateHash } from "../hash/calcHash.js";
 import { goToPath } from "../navigation/goToPath.js";
 import { goUp } from "../navigation/goUp.js";
+import { printList } from "../navigation/printList.js";
 // import os from 'os';
 
 export function handleData (data) {
   // console.log('Вы ввели ', data);
+  const input = data.trim();
 
-  if (data.includes('.exit')) {
+  if (input === '.exit') {
     process.exit();
   }
 
   try {
-    if (data.startsWith('up')) {
+    if (input === 'up') {
       goUp()
       return
     }
 
-    if (data.startsWith('cd')) {
-      const path = data.replace('cd', '').trim()
+    if (input === 'ls') {
+      printList()
+      return
+    }
+
+    if (input.startsWith('cd')) {
+      const path = input.replace('cd', '').trim()
       goToPath(path)
       return
     }
 
-    if (data.startsWith('hash')) {
-      const path = data.replace('hash', '').trim()
+    if (input.startsWith('hash')) {
+      const path = input.replace('hash', '').trim()
       // console.log(os.homedir());
       calculateHash(path)
       return
